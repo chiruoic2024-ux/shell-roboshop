183f15b2b47b6a5dafd9c1a589333db412623004
chiranjeevi <chiruoic2024@gmail.com>
2025-09-25 02:17:07 +0530

    commiting into shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0f89fe1c50eaa13ba" # replace with your own SG
+ZONE_ID="Z1043828YHJ86FJSXALD" # replace with your ID
+DOMAIN_NAME="chiru1982.fun"
 
 for instance in $@
 do
@@ -10,8 +12,29 @@ do
     #Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.chiru1982.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # chiru1982.fun
     fi
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set – Chiranjeevi DNS"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
