cfcbe7b5178790f7390cd0c80a2de51ebe85c80e
chiranjeevi <chiruoic2024@gmail.com>
2025-09-25 01:41:26 +0530

    commiting into shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-0f89fe1c50eaa13ba" # replace with your own SG
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids SG_ID   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     #Get Private IP
     if [ $instance != "frontend" ]; then
