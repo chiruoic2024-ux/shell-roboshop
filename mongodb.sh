2f607b7ff00506dfcf751407530774744d0e3e7a
chiranjeevi <chiruoic2024@gmail.com>
2025-09-25 20:42:50 +0530

    mongodb installation

---
+++ mongodb.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+USER_ID=$(id -u)
+R="\e[31m "
+G="\e[32m "
+Y="\e[33m "
+N="\e[0m  "
+
+LOGS_DIR="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_DIR/$SCRIPT_NAME.log"  # /var/log/shell-roboshop/mongodb.log
+mkdir -p $LOGS_DIR
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR::Install the softwares using root user priveleges!"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # Functions receives inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 .....$R Failure $N" | tee -a $LOG_FILE
+    exit 1 # failure is other than 0
+    else
+    echo -e "$2 .....$G Success $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo Repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDb"
+
+systemctl enable mongodb &>>$LOG_FILE
+VALIDATE $? "Enable MongoDb"
+
+systemctl start mongodb &>>$LOG_FILE
+VALIDATE $? "Start MongoDb"
